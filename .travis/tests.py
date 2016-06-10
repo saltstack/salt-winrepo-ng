53797af0da240b50c72c439b4e1da8b3894ee6f9
moebiuseye <moebius.eye@gmail.com>
2016-06-10 11:56:03 +0200

    Final line gives status of test.

--- .travis/tests.py
+++ .travis/tests.py
@@ -7,15 +7,18 @@ from jinja2 import Template
 from urlparse import urlparse
 import pycurl as curl
 
+teststatus=True
+
 def process_each(softwares):
+    global teststatus
     #pprint(softwares)
-    for _,software in softwares.items():
+    for s,software in softwares.items():
         try:
             if software['skip_urltest']:
                 continue
         except KeyError:
             pass
-        for _,version in software.items():
+        for v,version in software.items():
             try:
                 if version['skip_urltest']:
                     continue
@@ -34,7 +37,11 @@ def process_each(softwares):
                 try:
                     C.perform()
                     #assert C.getinfo(curl.HTTP_CODE) != 404, "[ERROR]\tURL returned code 404. File Missing? "
-                    print ( "Link alive? ", C.getinfo(curl.HTTP_CODE) != 404 )
+                    httpcode = C.getinfo(curl.HTTP_CODE)
+                    if httpcode == 404:
+                        # This build is failing !
+                        print("404 HERE : %s -- %s -- %s " % ( s, v, version['installer'] ) )
+                        teststatus=False
                 except curl.error as e:
                     errno, errstr = e
                     print(errno, errstr)
@@ -56,3 +63,6 @@ for cpuarch in ['AMD64', 'x86']:
                 continue
             data = yaml.load(yml)
             process_each(data)
+print("-"*80)
+assert teststatus, "BUILD FAILING. You can grep for '404 HERE' to find out how to fix this. "
+print("Everything went smoothly. No 404 errors were found. Happy deployment! ")
