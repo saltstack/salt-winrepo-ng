745864496d2fd4bf2703ee573fb7fba130df9a91
Bradley Jokinen <bradley.jokinen@gmail.com>
2018-07-31 13:56:05 -0500

    Fix Erlang version detection and add version 21.0.1

---
+++ erlang/install.cmd
@@ -0,0 +1,17 @@
+:: Installs Erlang
+:: Get the passed URL
+set "downURL=%~1"
+set "instNAME=%~2"
+set "progVER=%~3"
+set "regNAME=%~4"
+set "instFLAG=%~5"
+set DIR=%~dp0%
+
+:: Download Installer
+%systemroot%\System32\WindowsPowerShell\v1.0\powershell.exe -NoProfile -ExecutionPolicy Bypass -Command "((new-object net.webclient).DownloadFile('%downURL%','%instNAME%'))"
+
+:: Run Installer
+%systemroot%\System32\WindowsPowerShell\v1.0\powershell.exe -NoProfile -ExecutionPolicy Bypass -Command "& '%DIR%%instNAME%' %instFLAG%"
+
+:: Make Registry Entries - add important version entry that original installer does NOT provide
+reg add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\%regNAME%" /v DisplayVersion /d %progVER% /f
