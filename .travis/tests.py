e1724d76f6fd945cac286bd665e1daf7921252ae
Dafydd Jones <dafydd@techneg.it>
2020-12-09 02:35:15 +0000

    test: use python-magic where no Content-Type exists in response headers

--- .travis/tests.py
+++ .travis/tests.py
@@ -2,6 +2,7 @@
 import getopt
 import git
 import glob
+import magic
 import pycurl as curl
 import sys
 import traceback
@@ -92,15 +93,17 @@ def process_each(softwares):
             scheme = urlparse(version["installer"]).scheme
             if scheme in ["http", "https"]:
                 headers = BytesIO()
+                body = BytesIO()
                 printd("version['installer']", version["installer"])
                 c = curl.Curl()
                 # c.setopt(curl.WRITEFUNCTION, headers.write)
                 c.setopt(curl.URL, version["installer"])
-                c.setopt(curl.NOBODY, True)
                 c.setopt(curl.FOLLOWLOCATION, True)
                 c.setopt(curl.CONNECTTIMEOUT, 2)
                 c.setopt(curl.TIMEOUT, 5)
-                c.setopt(c.HEADERFUNCTION, headers.write)
+                c.setopt(curl.WRITEHEADER, headers)
+                c.setopt(curl.WRITEDATA, body)
+                c.setopt(curl.RANGE, "0-2047")
                 try:
                     c.perform()
                     # assert C.getinfo(curl.HTTP_CODE) != 404, "[ERROR]\tURL returned code 404. File Missing? "
@@ -116,7 +119,7 @@ def process_each(softwares):
                             ]
                         )["Content-Type"]
                     except Exception:
-                        content_type = "None/None"
+                        content_type = magic.from_buffer(body.getvalue(), mime=True)
                     count_c_types[content_type] += 1
                     printd("content_type:", content_type)
                     http_failure = False
