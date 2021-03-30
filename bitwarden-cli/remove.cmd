cf2eabda82663c362a71db849a911a503a3849a2
TheBigBear <TheBigBear@users.noreply.github.com>
2021-03-30 19:18:40 +0200

    Update remove.cmd

--- bitwarden-cli/remove.cmd
+++ bitwarden-cli/remove.cmd
@@ -4,7 +4,8 @@
 If Exist "%ProgramFiles%\Bitwarden CLI" rd /S /Q "%ProgramFiles%\Bitwarden CLI"
 
 :: Remove from Machine PATH
-setx /M PATH "%PATH:%%ProgramFiles%%\Bitwarden CLI;=%"
+:: setx /M PATH "%PATH:%%ProgramFiles%%\Bitwarden CLI;=%"
+:: setx /M PATH "%PATH:C:\Program Files\Bitwarden CLI;=%"
 
 :: Remove registry entries
 reg delete HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\Bitwarden-CLI /f
