a319bee8b7b598b78515af0f29c7780ad47cfee0
TheBigBear <TheBigBear@users.noreply.github.com>
2021-04-10 15:04:34 +0200

    Update remove.cmd

--- pfsensebackup/remove.cmd
+++ pfsensebackup/remove.cmd
@@ -1,7 +1,7 @@
 :: Script to uninstall pfSenseBackup
 
 :: Remove directories
-If Exist "%ProgramFiles%\pfSenseBackup" del /f /Q "%ProgramFiles%\pfSenseBackup\pfsense*.exe"
+If Exist "%ProgramFiles(x86)%\pfSenseBackup" del /f /Q "%ProgramFiles(x86)%\pfSenseBackup\pfsense*.exe"
 :: If Exist "%ProgramData%\pfSenseBackup" rd /S /Q "%ProgramData%\Auditbeat"
 
 :: Remove registry entries
