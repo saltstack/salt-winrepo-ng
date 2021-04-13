d37f0c82635a90e39358d291048104b8b302971e
TheBigBear <TheBigBear@users.noreply.github.com>
2021-04-13 10:04:54 +0200

    Update remove.cmd

--- pfsensebackup/remove.cmd
+++ pfsensebackup/remove.cmd
@@ -2,6 +2,7 @@
 
 :: Remove directories
 If Exist "%ProgramFiles(x86)%\pfSenseBackup\pfSenseBackup.exe" del /F /Q "%ProgramFiles(x86)%\pfSenseBackup\pfsensebackup.exe"
+If Exist "%ProgramFiles(x86)%\pfSenseBackup\remove.cmd" del /F /Q "%ProgramFiles(x86)%\pfSenseBackup\remove.cmd"
 
 :: Remove registry entries
 reg delete HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\pfSenseBackup /f
